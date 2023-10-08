9e85554508649153122b5f59b1dd62d37ca227c1
idhaya prasanth <109224918+idhayaprasanth@users.noreply.github.com>
2023-10-08 13:07:32 +0530

    Delete busapp directory

--- busapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from busapp.models import create_bus
-
-# Register your models here.
-admin.site.register(create_bus)
-class create_bus(admin.ModelAdmin):
-    bus_details=['disno','routeno','busroute','bustype','image','bustime','busstops','stop1','stop2','stop3','stop4','stop5']
